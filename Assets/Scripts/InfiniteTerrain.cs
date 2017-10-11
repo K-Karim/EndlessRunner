2a16b1b17b44f839caaf5c24c69527db7b396774
Taylbr <taylbr97@gmail.com>
2017-10-11 16:56:15 +1100

    Made ocean repeat (buggy though)

---
+++ Assets/Scripts/InfiniteTerrain.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System.Collections;
+
+public class InfiniteTerrain : MonoBehaviour
+{
+	public GameObject Player;
+	public GameObject[] seas;
+	private GameObject currSea;
+	private GameObject leftSea;
+	private GameObject topSea;
+	private GameObject diagSea;
+	private double diagDistance;
+	private double distance;
+	private float spacing;
+	private bool hasUpdated = true;
+	private float yPos;
+	
+	void Start ()
+	{
+		currSea = seas [0];
+		leftSea = seas [1];
+		topSea = seas [2];
+		diagSea = seas [3];
+		yPos = currSea.transform.position.y;
+		distance = Mathf.Abs(Player.transform.position.x - leftSea.transform.position.x);
+		diagDistance = Vector3.Distance (Player.transform.position, diagSea.transform.position);
+		spacing = Vector3.Distance (currSea.transform.position, topSea.transform.position);
+	}
+	
+	void Update ()
+	{
+		checkIfCurr (leftSea);
+		checkIfCurr (topSea);
+		checkIfCurr (diagSea);
+
+		if(!hasUpdated)
+			updatePos ();
+	}
+
+	void checkIfCurr(GameObject sea){
+
+		if (Mathf.Abs(Player.transform.position.x - sea.transform.position.x) < distance*.3
+			&& Mathf.Abs(Player.transform.position.z - sea.transform.position.z) < distance*.3) {
+			GameObject tempSea = currSea;
+			currSea = sea;
+			sea = tempSea;
+			hasUpdated = false;
+		}
+	}
+
+	void updatePos(){
+		if (Player.transform.position.x < currSea.transform.position.x
+			&& Player.transform.position.z > currSea.transform.position.z) {
+			Vector3 newPos = new Vector3 (currSea.transform.position.x - spacing, yPos, currSea.transform.position.z); 
+			leftSea.transform.position = newPos;
+
+			newPos = new Vector3 (currSea.transform.position.x, yPos, currSea.transform.position.z + spacing); 
+			topSea.transform.position = newPos;
+
+			newPos = new Vector3 (currSea.transform.position.x - spacing, yPos, currSea.transform.position.z + spacing); 
+			diagSea.transform.position = newPos;
+			hasUpdated = true;
+		} else if (Player.transform.position.x < currSea.transform.position.x) {
+			Vector3 newPos = new Vector3 (currSea.transform.position.x - spacing, yPos, currSea.transform.position.z); 
+			leftSea.transform.position = newPos;
+		} else if (Player.transform.position.z > currSea.transform.position.z) {
+			Vector3 newPos = new Vector3 (currSea.transform.position.x, yPos, currSea.transform.position.z + spacing); 
+			topSea.transform.position = newPos;
+		}
+	}
+}
