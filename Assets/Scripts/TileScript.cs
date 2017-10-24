3d84ffe04ea754b01b52f8b03711dad58db1d539
Karim Khairat <kkhairat@student.unimelb.edu.au>
2017-10-24 16:13:02 +1100

    Update TileScript.cs

--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Graphics and Interaction (COMP30019) 
  * Project 2: Endless Runner
  * Team: Karim Khairat, Duy (Daniel) Vu, and Brody Taylor
@@ -12,7 +12,6 @@ using UnityEngine;
 
 
 public class TileScript : MonoBehaviour {
-	private float delay = 0f;
 
 	private static TileScript tilescript;
 
@@ -40,21 +39,15 @@ public class TileScript : MonoBehaviour {
 		}
 	}
 
-	/* After player exits square,trigger FallDown, which makes squares fall after a delay */
+	/* After player exits square, make the squares falldown*/
 	void OnTriggerExit(Collider collider){
 		if (collider.tag == "Player") {
-            GetComponent<Rigidbody>().isKinematic = false;
+  		    //By setting isKinematic to false, isGravity will pull the square downn!
+        	    GetComponent<Rigidbody>().isKinematic = false;
         }
 
     }
 
-	/* Make square fall down after a delay */
-	IEnumerator FallDown()
-	{
-		// Wait the specified delay period before dropping squares!
-		yield return new WaitForSeconds (delay);
-		//By setting isKinematic to false, isGravity will pull the square downn!
-		GetComponent<Rigidbody> ().isKinematic = false;
-	}
+
 
 }
